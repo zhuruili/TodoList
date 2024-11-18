4b9c699d1d365074f51b23512c2bd77003b94dc3
RookieNoob <lizhurui20040305@outlook.com>
2024-11-18 23:41:11 +0800

    完善任务的添加、同步、删除功能

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import React from 'react';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
 function App(props) {
 
-  const taskList = props.tasks.map((task)=> (
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks?.map((task)=> (
     <Todo 
     id={task.id} 
     name={task.name} 
     completed={task.completed}
     key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted} 
+    deleteTask={deleteTask}
     />
   ));
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = {
+      id:`todo-${nanoid()}`,
+      name, 
+      completed: false
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      // if this task has a different ID, return it as is
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    // 如果id与传入的id不等，则保留，否则过滤
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks)
+  }
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
