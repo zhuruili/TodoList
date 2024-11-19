ea34da7de56d5ad176b4bda40df982d1a5200c41
RookieNoob <lizhurui20040305@outlook.com>
2024-11-19 22:58:44 +0800

    基本功能开发完全

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed, 
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
-  const taskList = tasks?.map((task)=> (
+  const [filter, setFilter] = useState('All');
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task)=> (
     <Todo 
     id={task.id} 
     name={task.name} 
@@ -18,6 +27,14 @@ function App(props) {
     toggleTaskCompleted={toggleTaskCompleted} 
     deleteTask={deleteTask}
     editTask={editTask}
+    />
+  ));
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+    key={name} 
+    name={name} 
+    isPressed={name === filter}
+    setFilter={setFilter}  //这里在第一次写的时候=之前的filter写成了fliter，之后报错说props.setFilter is not a function,这说明它只认识你传入的等号之前的内容
     />
   ));
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
@@ -69,9 +86,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
